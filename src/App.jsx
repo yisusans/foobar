de90f28683b56ef90d33397997c1940a255e9a97
Susan Yi <susan@okcupid.com>
2021-10-13 14:17:52 -0400

    initial set up with eslint and prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+export const foo = {
+  1: 'one',
+  2: '123',
+  3: '1231231312',
+  4: 'asdfasdf',
+  123123: '123',
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
